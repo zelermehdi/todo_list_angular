a48fd689909b7d4d3954d8d3c20005c9d37e9984
zelermehdi <gamouh.mehdi@gmail.com>
2023-08-23 14:24:54 +0200

    changement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Assurez-vous que cette ligne est présente
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
